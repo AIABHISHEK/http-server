0e09afe21ca4030dd900cc3ecc2fdfc4fe0f9206
ABHISHEK <ay22oct02@gmail.com>
2025-05-24 13:45:32 +0530

    url parsing

--- app/main.js
+++ app/main.js
@@ -12,6 +12,23 @@ const response =
     body;
 const server = net.createServer((socket) => {
     socket.write(response);
+    socket.on("data", (data) => {
+        console.log("Received data:", data.toString());
+        // read url path
+        const request = data.toString();
+        const [requestLine] = request.split('\r\n'); //we get the first line of http req
+        const [method, path, httpVersion] = requestLine.split(' '); //we split the first line by space
+        console.log("Method:", method);
+        console.log("Path:", path);
+        if (path === '/') {
+            const response = "HTTP/1.1 200 OK\r\n";
+            socket.write(response);
+        } else if (path.length > 0) {
+            const response = "HTTP/1.1 404 Not Found\r\n";
+            socket.write(response);
+            console.log("No URL path found in the request.");
+        }
+    });
     socket.on("close", () => {
         socket.end();
     })
