3db1ea93bfe747d1cf985567371b3636dcaa6253
ABHISHEK <ay22oct02@gmail.com>
2025-05-31 23:44:31 +0530

    updated

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 const net = require("net");
 const fs = require("fs");
-const { parseHttpRequest, buildResponse } = require("./util/parse_request.js");
+const { parseHttpRequest, buildResponse, endOfRequest } = require("./util/parse_request.js");
 const PATH = require('path');
 
 console.log("Logs from your program will appear here!");
@@ -9,49 +9,160 @@ const response =
     'HTTP/1.1 200 OK\r\n' +
     'Content-Type: text/plain\r\n' +
     `Content-Length: ${body.length}\r\n` +
-    'Connection: close\r\n' +
+    // 'Connection: close\r\n' +
     '\r\n' +
     body;
-const server = net.createServer((socket) => {
-    // socket.write(response);
-    socket.on("data", (data) => {
-        console.log("Received data:", data.toString());
-        // read url path
-        const request = data.toString();
-        const {
-            method,
-            path,
-            httpVersion,
-            headers,
-            body
-        } = parseHttpRequest(request);
-        console.log("Parsed request:", {
-            method,
-            path,
-            httpVersion,
-            headers,
-            body
-        });
-        console.log("Request header user-agent:", headers["user-agent"]);
-        console.log("Method:", method);
-        console.log("Path:", path);
-        if (path === '/') {
-            const body = 'Hello, world!';
-            const response = buildResponse({statusCode:200, headers:{}, body}, headers);
-            socket.write(response);
-        }
-        else if (path === '/files') {
-            console.log("Request for /files");
-            // const filePath = path.replace('/files/', '');
-            const fullPath = "data.txt";
-            const filePath = PATH.join(__dirname, fullPath);
-            console.log("File path:", filePath);
-            fs.stat(filePath, (err, stats) => {
-                console.log("File path:", fullPath);
-                console.log("File stats:", stats);
-                console.log("Error:", err);
-                if (err || !stats.isFile()) {
-                    const body = 'File Not Found';
+    const server = net.createServer((socket) => {
+        console.log("New connection established");
+        // socket.write(response);
+        let buffer = Buffer.alloc(0);
+        socket.on("data", (data) => {
+            buffer = Buffer.concat([buffer, data]);
+            console.log("Received data:", data.toString());
+            // read url path
+            // if req complete we will proceed
+            let endOfReq;
+            while ((endOfReq = endOfRequest(buffer)) !== -1) {
+                // const request = data.toString();
+                const request = buffer.subarray(0, endOfReq).toString(); // +4 for \r\n\r\n
+                // remove the processed part from the buffer
+                buffer = buffer.subarray(endOfReq); // 4 for \r\n\r\n
+                const {
+                    method,
+                    path,
+                    httpVersion,
+                    headers,
+                    body
+                } = parseHttpRequest(request);
+                console.log("Parsed request:", {
+                    method,
+                    path,
+                    httpVersion,
+                    headers,
+                    body
+                });
+                console.log("Request header user-agent:", headers["user-agent"]);
+                console.log("Method:", method);
+                console.log("Path:", path);
+                if (path === '/') {
+                    const body = 'Hello, world!';
+                    const response = buildResponse({ statusCode: 200, headers: {}, body }, headers);
+                    socket.write(response);
+                }
+                else if (path === '/files') {
+                    console.log("Request for /files");
+                    // const filePath = path.replace('/files/', '');
+                    const fullPath = "data.txt";
+                    const filePath = PATH.join(__dirname, fullPath);
+                    console.log("File path:", filePath);
+                    fs.stat(filePath, (err, stats) => {
+                        console.log("File path:", fullPath);
+                        console.log("File stats:", stats);
+                        console.log("Error:", err);
+                        if (err || !stats.isFile()) {
+                            const body = 'File Not Found';
+                            const response =
+                                'HTTP/1.1 404 Not Found\r\n' +
+                                'Content-Type: text/plain\r\n' +
+                                `Content-Length: ${body.length}\r\n` +
+                                'Connection: close\r\n' +
+                                '\r\n' +
+                                body;
+                            socket.write(response);
+                            socket.end();
+                        } else {
+                            fs.readFile(filePath, (err, data) => {
+                                if (err) {
+                                    console.error("Error reading file:", err);
+                                    const body = 'Internal Server Error';
+                                    const response =
+                                        'HTTP/1.1 500 Internal Server Error\r\n' +
+                                        'Content-Type: text/plain\r\n' +
+                                        `Content-Length: ${body.length}\r\n` +
+                                        'Connection: close\r\n' +
+                                        '\r\n' +
+                                        body;
+                                    socket.write(response);
+                                    socket.end();
+                                    return;
+                                }
+                                // console.log("File data:", data.toString());
+                                console.log("File data:", data);
+                                const response =
+                                    'HTTP/1.1 200 OK\r\n' +
+                                    'Content-Type: application/octet-stream\r\n' +
+                                    `Content-Length: ${stats.size}\r\n` +
+                                    'Connection: keep-alive\r\n' +
+                                    '\r\n' +
+                                    data;
+                                socket.write(response);
+                            });
+                        }
+                    });
+                }
+                else if (path.startsWith('/files/') && path.length > 7) {
+                    console.log("Request for /files/");
+                    // create a file with the given name after
+                    const fileName = path.slice(7); // Extract the file name from the path
+                    const fullPath = PATH.join(__dirname, fileName);
+                    console.log("File path:", fullPath);
+                    fs.writeFile(fullPath, body, (err) => {
+                        if (err) {
+                            console.error("Error writing file:", err);
+                            const response =
+                                'HTTP/1.1 500 Internal Server Error\r\n' +
+                                'Content-Type: text/plain\r\n' +
+                                'Connection: close\r\n' +
+                                '\r\n' +
+                                'Internal Server Error';
+                            socket.write(response);
+                        } else {
+                            const response =
+                                'HTTP/1.1 201 Created\r\n' +
+                                'Content-Type: text/plain\r\n' +
+                                `Content-Length: ${body.length}\r\n` +
+                                'Connection: close\r\n' +
+                                '\r\n' +
+                                body;
+                            socket.write(response);
+                        }
+                    });
+                }
+                else if (path === '/user-agent') {
+                    const body = headers["user-agent"];
+                    const response =
+                        'HTTP/1.1 200 OK\r\n' +
+                        'Content-Type: text/plain\r\n' +
+                        `Content-Length: ${body.length}\r\n` +
+                        // 'Connection: close\r\n' +
+                        '\r\n' +
+                        body;
+                    socket.write(response);
+                }
+                else if (path === '/echo') {
+                    const body = 'Abc echo res!';
+                    const response =
+                        'HTTP/1.1 200 OK\r\n' +
+                        'Content-Type: text/plain\r\n' +
+                        `Content-Length: ${body.length}\r\n` +
+                        // 'Connection: close\r\n' +
+                        '\r\n' +
+                        body;
+                    socket.write(response);
+                }
+                else if (path.startsWith('/echo/')) {
+                    const body = 'Abc echo res!';
+                    const response =
+                        'HTTP/1.1 200 OK\r\n' +
+                        'Content-Type: text/plain\r\n' +
+                        `Content-Length: ${body.length}\r\n` +
+                        // 'Connection: close\r\n' +
+                        '\r\n' +
+                        body;
+                    socket.write(response);
+                }
+                else {
+                    const body = 'Not Found';
                     const response =
                         'HTTP/1.1 404 Not Found\r\n' +
                         'Content-Type: text/plain\r\n' +
@@ -60,110 +171,10 @@ const server = net.createServer((socket) => {
                         '\r\n' +
                         body;
                     socket.write(response);
-                    socket.end();
-                } else {
-                    fs.readFile(filePath, (err, data) => {
-                        if (err) {
-                            console.error("Error reading file:", err);
-                            const body = 'Internal Server Error';
-                            const response =
-                                'HTTP/1.1 500 Internal Server Error\r\n' +
-                                'Content-Type: text/plain\r\n' +
-                                `Content-Length: ${body.length}\r\n` +
-                                'Connection: close\r\n' +
-                                '\r\n' +
-                                body;
-                            socket.write(response);
-                            socket.end();
-                            return;
-                        }
-                        // console.log("File data:", data.toString());
-                        console.log("File data:", data);
-                        const response =
-                            'HTTP/1.1 200 OK\r\n' +
-                            'Content-Type: application/octet-stream\r\n' +
-                            `Content-Length: ${stats.size}\r\n` +
-                            'Connection: keep-alive\r\n' +
-                            '\r\n' +
-                            data;
-                        socket.write(response);
-                    });
+                    console.log("No URL path found in the request.");
                 }
-            });
-        }
-        else if (path.startsWith('/files/') && path.length > 7) {
-            console.log("Request for /files/");
-            // create a file with the given name after
-            const fileName = path.slice(7); // Extract the file name from the path
-            const fullPath = PATH.join(__dirname, fileName);
-            console.log("File path:", fullPath);
-            fs.writeFile(fullPath, body, (err) => {
-                if (err) {
-                    console.error("Error writing file:", err);
-                    const response =
-                        'HTTP/1.1 500 Internal Server Error\r\n' +
-                        'Content-Type: text/plain\r\n' +
-                        'Connection: close\r\n' +
-                        '\r\n' +
-                        'Internal Server Error';
-                    socket.write(response);
-                } else {
-                    const response =
-                        'HTTP/1.1 201 Created\r\n' +
-                        'Content-Type: text/plain\r\n' +
-                        `Content-Length: ${body.length}\r\n` +
-                        'Connection: close\r\n' +
-                        '\r\n' +
-                        body;
-                    socket.write(response);
-                }
-            });
-        }
-        else if (path === '/user-agent') {
-            const body = headers["user-agent"];
-            const response =
-                'HTTP/1.1 200 OK\r\n' +
-                'Content-Type: text/plain\r\n' +
-                `Content-Length: ${body.length}\r\n` +
-                'Connection: close\r\n' +
-                '\r\n' +
-                body;
-            socket.write(response);
-        }
-        else if (path === '/echo') {
-            const body = 'Abc echo res!';
-            const response =
-                'HTTP/1.1 200 OK\r\n' +
-                'Content-Type: text/plain\r\n' +
-                `Content-Length: ${body.length}\r\n` +
-                'Connection: close\r\n' +
-                '\r\n' +
-                body;
-            socket.write(response);
-        }
-        else if (path.startsWith('/echo/')) {
-            const body = 'Abc echo res!';
-            const response =
-                'HTTP/1.1 200 OK\r\n' +
-                'Content-Type: text/plain\r\n' +
-                `Content-Length: ${body.length}\r\n` +
-                'Connection: close\r\n' +
-                '\r\n' +
-                body;
-            socket.write(response);
-        }
-        else {
-            const body = 'Not Found';
-            const response =
-                'HTTP/1.1 404 Not Found\r\n' +
-                'Content-Type: text/plain\r\n' +
-                `Content-Length: ${body.length}\r\n` +
-                'Connection: close\r\n' +
-                '\r\n' +
-                body;
-            socket.write(response);
-            console.log("No URL path found in the request.");
-        }
+            }
+        
     });
     socket.on("close", () => {
         socket.end();
--- app/util/parse_request.js
+++ app/util/parse_request.js
@@ -1,3 +1,5 @@
+import zlib from 'zlib';
+
 function getStatusText(code) {
     const statusTexts = {
         200: "OK",
@@ -30,25 +32,29 @@ export function buildResponse({statusCode, headers = {}, body = ''}, incomingHea
     console.log('Building response with status code:', statusCode);
     const defaultHeaders = {
         'Content-Type': 'text/plain',
-        'Connection': 'close',
+        // 'Connection': 'close',
         'Content-Length': Buffer.byteLength(body).toString()
     };
-
+    let responseBody = body;
     const finalHeaders = { ...defaultHeaders, ...headers };
     if (incomingHeaders['accept-encoding']) {
-        if( incomingHeaders['accept-encoding'].includes('gzip')) {
+        if (incomingHeaders['accept-encoding'].includes('gzip')) {
+            responseBody = zlib.gzipSync(Buffer.from(body));
             finalHeaders['Content-Encoding'] = 'gzip';
-            // Note: In a real implementation, you would compress the body here
-            // For now, we will just set it as is
+            finalHeaders['Content-Length'] = responseBody.length.toString();
         }
     }
+
     const response = `HTTP/1.1 ${statusCode} ${getStatusText(statusCode)}\r\n` +
         Object.entries(finalHeaders)
             .map(([key, value]) => `${key}: ${value}`)
             .join('\r\n') +
-        '\r\n\r\n' +
-        body;
-    return response;
+        '\r\n' +
+        '\r\n';
+    // concating buffer with string will result in string which will corrupt the binary we got from compression
+    return Buffer.isBuffer(responseBody)
+        ? Buffer.concat([Buffer.from(response), responseBody])
+        : response + responseBody;;
 }
 
 
@@ -87,4 +93,25 @@ export function parseHttpRequest(raw) {
         headers,
         body
     };
+}
+
+export function endOfRequest(buffer) {
+    // convert to string if buffer is not a string
+    buffer = buffer.toString();
+    // Check if the buffer ends with \r\n\r\n
+    const endOfHeaders = buffer.indexOf('\r\n\r\n');
+    if (endOfHeaders === -1) {
+        return -1; // No end of headers found
+    }
+    //check content-length header
+    const contentLength = buffer.toString().match(/Content-Length:\s*(\d+)/i);
+    if (contentLength) {
+        const length = parseInt(contentLength[1], 10);
+        // Check if the body is present based on Content-Length
+        if (buffer.length >= endOfHeaders + 4 + length) {
+            return endOfHeaders + 4 + length; // Return the end of the request
+        }
+        return -1; // Not enough data for the body
+    }
+    return endOfHeaders + 4;
 }
